ab72c54a480fd8128cf2431ad9e0b1a85c71fe8b
Rohan4404 <rohansharma99anc@gmail.com>
2023-12-16 15:26:30 +0530

    db -connect

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
-require("./db/conn");
 const router = require("./routers/men");
 const MensRanking = require("./models/mans");
 const app = express();
-require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+require("./db/conn");
 app.use(express.json());
 app.use(router);
 
